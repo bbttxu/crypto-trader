e105b7649331540d929d5d0644d5e2fec6e5c7be
Adam Krawiec <adamkrawiec@gmail.com>
2017-12-05 18:01:31 -0600

    probably over-thinking configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,64 +1,77 @@
+
+const {
+  map,
+  merge,
+  concat
+} = require( 'ramda' );
+
+const defaults = {
+  script: "single.coffee",
+  interpreter: './node_modules/coffee-script/bin/coffee',
+  watch: false,
+  max_restarts: 10,
+  max_memory_restart: '150M'
+}
+
+const setDefaults = ( app )=> {
+  return merge( defaults, app )
+}
+
+const applyRandomReload = ( app )=> {
+  return merge(
+    app,
+    {
+      restart_delay: Math.round(
+        (
+          45 + (
+            Math.random() * 30
+          )
+        )
+      ) * 1000,
+    }
+  )
+}
+
+const setupProduct = ( product )=> {
+  return {
+    name: product,
+    env: {
+      PRODUCT_ID: product
+    }
+  }
+}
+
 module.exports = {
   /**
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
-
-    // First application
-    {
-      name: "Fills",
-      script: "fills.coffee",
-      interpreter: './node_modules/coffee-script/bin/coffee',
-      watch: true
-    },
-    {
-      name: "BTC-USD",
-      script: "single.coffee",
-      interpreter: './node_modules/coffee-script/bin/coffee',
-      watch: true,
-      env: {
-        PRODUCT_ID: 'BTC-USD'
+  apps: concat(
+    [
+      // First application
+      {
+        name: "Fills",
+        script: "fills.coffee",
+        interpreter: './node_modules/coffee-script/bin/coffee',
+        watch: true
       }
-    },
-    {
-      name: "ETH-USD",
-      script: "single.coffee",
-      interpreter: './node_modules/coffee-script/bin/coffee',
-      watch: true,
-      env: {
-        PRODUCT_ID: 'ETH-USD'
-      }
-    },
-    {
-      name: "LTC-USD",
-      script: "single.coffee",
-      interpreter: './node_modules/coffee-script/bin/coffee',
-      watch: true,
-      env: {
-        PRODUCT_ID: 'LTC-USD'
-      }
-    },
-    {
-      name: "ETH-BTC",
-      script: "single.coffee",
-      interpreter: './node_modules/coffee-script/bin/coffee',
-      watch: true,
-      env: {
-        PRODUCT_ID: 'ETH-BTC'
-      }
-    },
-    {
-      name: "LTC-BTC",
-      script: "single.coffee",
-      interpreter: './node_modules/coffee-script/bin/coffee',
-      watch: true,
-      env: {
-        PRODUCT_ID: 'LTC-BTC'
-      }
-    }
-  ],
-
-  deploy : {
-  }
+    ],
+    map(
+      setDefaults,
+      map(
+        applyRandomReload,
+        map(
+          setupProduct,
+          [
+            'BTC-USD',
+            'ETH-USD',
+            'LTC-USD',
+            'ETH-BTC',
+            'LTC-USD'
+          ]
+        )
+      )
+    )
+  ),
+  deploy : {}
 }
