408ae169180e40b54616eeae4312d5b05c77de81
Adam Krawiec <adamkrawiec@gmail.com>
2017-12-29 17:57:34 -0600

    update paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ const {
 
 const defaults = {
   script: "single.coffee",
-  interpreter: './node_modules/coffee-script/bin/coffee',
+  interpreter: './node_modules/coffeescript/bin/coffee',
   watch: false,
   max_restarts: 10,
   max_memory_restart: '1000M'
@@ -80,7 +80,7 @@ module.exports = {
               {
                 name: "Fills",
                 script: "fills.coffee",
-                interpreter: './node_modules/coffee-script/bin/coffee',
+                interpreter: './node_modules/coffeescript/bin/coffee',
                 watch: true
               }
             ],
